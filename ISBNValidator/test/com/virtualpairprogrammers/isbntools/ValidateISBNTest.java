19eaa18226ac4f6e47cb8433b7eb02595286300b
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-19 14:30:09 -0300

    Adicionando alguns exemplos

---
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -0,0 +1,24 @@
+package com.virtualpairprogrammers.isbntools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValidateISBNTest {
+
+    @Test
+    public void checkAValidISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("0140449116");
+        assertTrue("first value", result);
+        result = validator.checkISBN("0140177396");
+        assertTrue("second value", result);
+    }
+
+    @Test
+    public void checkAnInvalidISBN() {
+        ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN("0140449117");
+        assertFalse(result);
+    }
+}
